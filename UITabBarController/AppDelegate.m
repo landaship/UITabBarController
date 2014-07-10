2ca4c861138ddef6f6ba3aecadac743f2d30ad1d
landaship <landaship@163.com>
2014-07-10 23:07:46 +0800

    添加代理
    1.决定是否item被选中
    2.自定义item位置的代理

--- UITabBarController/AppDelegate.m
+++ UITabBarController/AppDelegate.m
@@ -12,6 +12,7 @@
 #import "ThirdViewController.h"
 #import "FourViewController.h"
 #import "FiveViewController.h"
+#import "SixViewController.h"
 
 @implementation AppDelegate
 
@@ -21,16 +22,17 @@
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     
-    // 1. 添加那5个viewcontroller
+    // 1. 添加那6个viewcontroller
     FirstViewController *firtVC = [[FirstViewController alloc]init];
     SecondViewController *secondVC = [[SecondViewController alloc]init];
     ThirdViewController *thirdVC = [[ThirdViewController alloc]init];
     FourViewController *fourVC = [[FourViewController alloc]init];
     FiveViewController *fiveVC = [[FiveViewController alloc]init];
+    SixViewController *sixVC = [[SixViewController alloc]init];
     
     // 2.   添加tabBarController
     UITabBarController *tabBarController = [[UITabBarController alloc]init];
-    tabBarController.viewControllers = [NSArray arrayWithObjects:firtVC, secondVC, thirdVC, fourVC, fiveVC, nil];
+    tabBarController.viewControllers = [NSArray arrayWithObjects:firtVC, secondVC, thirdVC, fourVC, fiveVC, sixVC, nil];
     
     // 3.1. 设置背景图片
     [tabBarController.tabBar setBackgroundImage:[UIImage imageNamed:@"tabbar_background.png"]];
@@ -41,19 +43,22 @@
     [thirdVC.tabBarItem setImage:[UIImage imageNamed:@"tabbar_profile_selected.png"]];
     [fourVC.tabBarItem setImage:[UIImage imageNamed:@"tabbar_discover.png"]];
 //    [fiveVC.tabBarItem setImage:[UIImage imageNamed:@"tabbar_more.png"]];
-    fiveVC.tabBarItem = [[UITabBarItem alloc]initWithTabBarSystemItem:UITabBarSystemItemMore tag:4];
+//    fiveVC.tabBarItem = [[UITabBarItem alloc]initWithTabBarSystemItem:UITabBarSystemItemMore tag:4];
+    fiveVC.tabBarItem = [[UITabBarItem alloc]initWithTabBarSystemItem:UITabBarSystemItemHistory tag:4];
+    sixVC.tabBarItem = [[UITabBarItem alloc]initWithTabBarSystemItem:UITabBarSystemItemDownloads tag:5];
     
     // 3.3 设置每个Item文字
     firtVC.title = @"首页";
     secondVC.title = @"消息";
     thirdVC.title = @"我";
     fourVC.title = @"广场";
-    fiveVC.title = @"更多";
+    fiveVC.title = @"历史";
+    sixVC.title = @"下载";
     
     // 3.4. 设置数量图标
     firtVC.tabBarItem.badgeValue = @"10";
-    secondVC.tabBarItem.badgeValue = @"11230";
-    thirdVC.tabBarItem.badgeValue = @"1543430";
+    secondVC.tabBarItem.badgeValue = @"meme";
+    thirdVC.tabBarItem.badgeValue = @"中文";
     
     // 3.5 设置选中时图标的颜色变化, 貌似这两个效果没有多大区别
     tabBarController.tabBar.tintColor = [UIColor greenColor];
@@ -64,8 +69,45 @@
     
     // 4. 添加根控制器
     self.window.rootViewController =  tabBarController;
+    
+    // 5. 添加代理
+    tabBarController.delegate = self;
+    
     [self.window makeKeyAndVisible];
     return YES;
+}
+
+#pragma mark - 设置能否被选中和确定那个按钮被选中
+
+// 确定按钮是否可以被选中
+- (BOOL)tabBarController:(UITabBarController *)tabBarController shouldSelectViewController:(UIViewController *)viewController
+{
+    return YES; // 都可以被选中
+}
+
+// 那个item被选中了？
+- (void)tabBarController:(UITabBarController *)tabBarController didSelectViewController:(UIViewController *)viewController
+{
+    NSLog(@"被选中的item是:%@",viewController);
+}
+
+#pragma mark - 自定义调节item位置
+// 开始编辑item位置
+- (void)tabBarController:(UITabBarController *)tabBarController willBeginCustomizingViewControllers:(NSArray *)viewControllers
+{
+    NSLog(@"编辑前controllers:%@", viewControllers);
+}
+
+// 点击了Done即将完成位置编辑
+- (void)tabBarController:(UITabBarController *)tabBarController willEndCustomizingViewControllers:(NSArray *)viewControllers changed:(BOOL)changed
+{
+    NSLog(@"即将完成编辑controllers:%@", viewControllers);
+}
+
+// 完成编辑
+- (void)tabBarController:(UITabBarController *)tabBarController didEndCustomizingViewControllers:(NSArray *)viewControllers changed:(BOOL)changed
+{
+    NSLog(@"完成编辑controllers:%@", viewControllers);
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
---
+++ UITabBarController/SixViewController.h
@@ -0,0 +1,13 @@
+//
+//  SixViewController.h
+//  UITabBarController
+//
+//  Created by Louis on 14-7-10.
+//  Copyright (c) 2014年 Louis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SixViewController : UIViewController
+
+@end
